527b64e4eefa41862ee1573d1c31632fbffa6f6d
YuTecHealth <67631044+YuTecHealth@users.noreply.github.com>
2020-08-12 12:32:59 +0800

    Update sw.js

--- LAB3/sw.js
+++ LAB3/sw.js
@@ -1,1 +1,19 @@
-console.log('Hello from sw.js');
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+// 存任何的css
+workbox.routing.registerRoute(
+  new RegExp('.*\.css'),
+  workbox.strategies.cacheFirst({
+    cacheName: 'css-cache'
+  })
+);
+// 存任何的images
+workbox.routing.registerRoute(
+  new RegExp('.*\.(?:png|jpg|jpeg|svg|gif)'),
+  workbox.strategies.cacheFirst({
+    cacheName: 'image-cache'
+  })
+);
+~
+~
+~
